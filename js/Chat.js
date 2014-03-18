5d37974966b8410d0aae3778ec0edba33d20b667
unknown <tico_fernandez_m@roble8.IESPAL>
2014-03-18 14:28:28 +0100

    Mejorada Interfaz & Login

--- js/Chat.js
+++ js/Chat.js
@@ -1,41 +1,50 @@
 $(function() {
-  var socket = io.connect("http://172.16.1.9:14494");
+	var host = "http://localhost";
+	var port = 14494;
+	var nick;
+	
+	var socket = io.connect(host+":"+port);
   
-  addLine("Conectando...");
+	addLine("Conectando a "+ host+":"+port +"...");
   
-  socket.on("connect", function() {
-      setConection(true);
-      socket.emit("nick", prompt("Nick?"));
-  });
+	socket.on("connect", function() {
+		setConection(true);
+		addEnter();
+		addLine("Esperando nick...");
+		nick = prompt("Nick?");
+		socket.emit("nick", nick);
+		addLine(nick);
+		addLine("...");
+	});
 
-  $('#msg-input input').keypress(function(e) {
-      if (e.which == 13) {
-          socket.emit("msg", $(this).val());
-          $(this).val('');
-      }
-  });
+	$('#msg-input input').keypress(function(e) {
+		if (e.which == 13) {
+			socket.emit("msg", $(this).val());
+			$(this).val('');
+		}
+	});
 
-  socket.on("msg", function(nick, msg) {
-     addLine("<strong>[" + nick + "]: </strong>" + msg);
-  });
-  
-  socket.on("report", function(msg) {
-     addLine("<strong>[Server]: </strong>" + msg);
-  });
-  
-  socket.on("action", function(actionName){
-    while(actionName == "repeatNick")
-    {
-      socket.emit("nick", prompt("Nick?"));
-    }
-  });
-  
-  socket.on("nicks", function(nicks) {
-      $("#users").html('');
-      for (var i = 0; i < nicks.length; i++) {
-          $(document.createElement("li")).text(nicks[i]).appendTo("#users");
-      }
-  });
+	socket.on("msg", function(nick, msg) {
+		addLine("<strong>[" + nick + "]: </strong>" + msg);
+	});
+
+	socket.on("report", function(msg) {
+		addLine("<strong>[Server]: </strong>" + msg);
+	});
+
+	socket.on("action", function(actionName){
+		while(actionName == "repeatNick")
+		{
+			socket.emit("nick", prompt("Nick?"));
+		}
+	});
+
+	socket.on("nicks", function(nicks) {
+		$("#users").html('');
+		for (var i = 0; i < nicks.length; i++) {
+			$(document.createElement("li")).text(nicks[i]).appendTo("#users");
+		}
+	});
 
 });
   
@@ -43,6 +52,14 @@ function addLine(msg)
 {
   $(document.createElement("div"))
   .html(msg)
+  .appendTo("#messages");
+
+  $('#messages-container').scrollTop($('#messages').height());
+}
+function addEnter()
+{
+  $(document.createElement("br"))
+  .html("")
   .appendTo("#messages");
 
   $('#messages-container').scrollTop($('#messages').height());
@@ -57,4 +74,9 @@ function setConection(state)
     document.getElementById("conection-status").style.color = "#ff0000";
     document.getElementById("conection-status").innerHTML = "Desconectado.";
   }
+}
+
+function showChat()
+{
+
 }
