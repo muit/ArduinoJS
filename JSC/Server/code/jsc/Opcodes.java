b1847abe7b411049c4886a814a876e0bc995cb27
unknown <tico_fernandez_m@roble8.IESPAL>
2014-04-23 09:30:05 +0200

    OpCodes

---
+++ JSC/Server/code/jsc/Opcodes.java
@@ -0,0 +1,20 @@
+
+
+/**
+ *
+ * @author Miguel_F
+ */
+public class Util {
+	enum static byte Main_Opcode
+	{
+		OP_MAIN_DEFAULT(0xFF);
+	}
+	
+	enum static byte Id_Opcode
+	{
+		OP_ID_DIGITAL_0(0x00),
+		OP_ID_DIGITAL_1(0x01),
+		OP_ID_DIGITAL_2(0x02),
+		OP_ID_DIGITAL_3(0x03),
+	}
+}
